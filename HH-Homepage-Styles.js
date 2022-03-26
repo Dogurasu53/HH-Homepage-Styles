67c460492754b77d8aaf0dd6096c7f0b79961826
Dogurasu53 <31224112+Dogurasu53@users.noreply.github.com>
2022-03-26 00:20:35 -0300

    Added template images so it's easier to add yours into the game

--- HH-Homepage-Styles.js
+++ HH-Homepage-Styles.js
@@ -14,6 +14,7 @@
 /*  ===========
      CHANGELOG
     =========== */
+// 0.1.1: changed the original images on the source code to actual template images
 // 0.1.0: uploaded the script to github
 // 0.0.3: optimized the code so it's easier for new users to use and so it's incredibly easy to add any amount of images to cycle randomly
 // 0.0.2: made it possible to cycle between 2 possible images instead of a static one, not too hard setting up for more than 2
@@ -21,10 +22,11 @@
 
 (() => {
     var sheetMM = (function() {var style = document.createElement('style');document.head.appendChild(style);return style.sheet;})();
-
+    
+    // Change the urls of the images below to any images you want, doesn't need to be 2, any amount will do, they cycle randomly when refreshing the page
     var pictures = [
-        "https://cdn.discordapp.com/attachments/337381881769361409/956528182805270568/ava0.png",
-        "https://cdn.discordapp.com/attachments/337381881769361409/956615463675232287/ava1.png",
+        "https://cdn.discordapp.com/attachments/337381881769361409/957115058025017354/StandingWomanImageTemplate.png",
+        "https://cdn.discordapp.com/attachments/337381881769361409/957115984261550160/SittingWomanImageTemplate.png",
     ];
     var r = Math.floor(Math.random() * pictures.length);
     sheetMM.insertRule('div.waifu-container img.avatar {content: url(' + pictures[r] + '); object-fit: cover;}');
